c59759623f354bd7f72c2c22d5f310f73b639254
melchisedech333 <melchisedech333@gmail.com>
2022-06-30 22:17:16 -0300

    Updates

--- background.js
+++ background.js
@@ -2,9 +2,8 @@
 (function() {
 
 	var url = window.location.href.toString().trim();
-    if (url.indexOf('youtube.com') == -1) {
+    if (url.indexOf('youtube.com') == -1) 
         return;
-    }
 
 	console.log("YouTube Speed Toolbar Started - Cristo <3");
 
